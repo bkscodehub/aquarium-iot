fe522da608888974af665686788e3d705586ba9a
karthik <karthikeyan.tandl@gmail.com>
2025-05-20 17:57:40 +0530

    add timestamp during commit

--- update-submodules.bat
+++ update-submodules.bat
@@ -31,7 +31,10 @@ git add subprojects\aquarium-iot-shared-lib
 
 echo.
 echo === Committing submodule updates ===
-git commit -m "Update submodules: to latest main $(Get-Date -Format 'yyyy-MM-dd-HH:mm:ss')"
+for /f "tokens=2 delims==" %A in ('wmic os get localdatetime /value') do set datetime=%A
+set datetime=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%-%datetime:~8,2%:%datetime:~10,2%:%datetime:~12,2%
+echo %datetime%
+git commit -m "Update submodules: to latest main %datetime%"
 
 echo.
 echo === Pushing to remote ===

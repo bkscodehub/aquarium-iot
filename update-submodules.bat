82e033a783b412c4a7dfb7793397e3bd6ab04758
karthik <karthikeyan.tandl@gmail.com>
2025-05-20 18:06:08 +0530

    Updated submodules and update script

--- update-submodules.bat
+++ update-submodules.bat
@@ -31,10 +31,7 @@ git add subprojects\aquarium-iot-shared-lib
 
 echo.
 echo === Committing submodule updates ===
-for /f "tokens=2 delims==" %A in ('wmic os get localdatetime /value') do set datetime=%A
-set datetime=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%-%datetime:~8,2%:%datetime:~10,2%:%datetime:~12,2%
-echo %datetime%
-git commit -m "Update submodules: to latest main %datetime%"
+git commit -m "Update submodules: to latest main"
 
 echo.
 echo === Pushing to remote ===

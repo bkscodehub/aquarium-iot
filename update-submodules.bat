bb60e9871b1bae11df07dbf9d8211e386c14dfc0
karthik <karthikeyan.tandl@gmail.com>
2025-05-22 08:53:57 +0530

    Updated submodule update script with commit message

--- update-submodules.bat
+++ update-submodules.bat
@@ -1,6 +1,15 @@
 @echo off
 SETLOCAL
 
+:: Check if a commit message argument is provided
+IF "%~1"=="" (
+    echo Error: Please provide a commit message as an argument.
+    echo Usage: update_submodules.bat "Your commit message"
+    exit /b 1
+)
+
+SET COMMIT_MESSAGE=%~1
+
 echo.
 echo === Updating submodule: aquarium-lighting-controller ===
 cd subprojects\aquarium-lighting-controller
@@ -31,7 +40,7 @@ git add subprojects\aquarium-iot-shared-lib
 
 echo.
 echo === Committing submodule updates ===
-git commit -m "Update submodules: to latest main"
+git commit -m "Update submodules: %COMMIT_MESSAGE%"
 
 echo.
 echo === Pushing to remote ===
